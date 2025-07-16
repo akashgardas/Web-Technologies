5e6dce0a23ee589906feb6e9fca2391ddf287720
Gardas Akash <akash39g@gmail.com>
2025-07-16 18:17:51 +0530

    Fine-tuning; DRY principle

--- My Works/Traffic Lights/script.js
+++ My Works/Traffic Lights/script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // 1. Button press Event
 // 2. Blink lights sequentially
@@ -6,59 +6,70 @@
 //      - Clear lights, Blink yellow lights
 //      - Clear lights, Blink green lights
 
-const clearLights = () => {
-    const trafficLights = document.querySelectorAll('.traffic-light');
+// Light Colors
+const RED = 0;
+const YELLOW = 1;
+const GREEN = 2;
+const lightClasses = ["red-light", "yellow-light", "green-light"];
 
-    for (let i = 0; i < 3; i ++) {
-        const lights = trafficLights[i].querySelectorAll('.light');
-        for(let j = 0; j < 3; j ++) {
-            lights[j].classList.remove('red-light');
-            lights[j].classList.remove('yellow-light');
-            lights[j].classList.remove('green-light');
-        }
+const trafficLights = document.querySelectorAll(".traffic-light");
+let lightColor = RED;
+
+const clearLights = () => {
+  for (let i = 0; i < 3; i++) {
+    const lights = trafficLights[i].querySelectorAll(".light");
+    for (let j = 0; j < 3; j++) {
+      lights[j].classList.remove("red-light");
+      lights[j].classList.remove("yellow-light");
+      lights[j].classList.remove("green-light");
     }
+  }
 };
 
-const blinkRedLights = () => {
-    const trafficLights = document.querySelectorAll('.traffic-light');
+const glowLights = (lightColor) => {
+  for (let i = 0; i < 3; i++) {
+    const lights = trafficLights[i].querySelectorAll(".light");
+    lights[lightColor].classList.add(lightClasses[lightColor]);
+  }
+};
 
-    for (let i = 0; i < 3; i ++) {
-        const lights = trafficLights[i].querySelectorAll('.light');
-        lights[0].classList.add('red-light');
-    }
-}
+/* 
+// DRY Principle
+
+const blinkRedLights = () => {
+  for (let i = 0; i < 3; i++) {
+    const lights = trafficLights[i].querySelectorAll(".light");
+    lights[0].classList.add("red-light");
+  }
+};
 
 const blinkYellowLights = () => {
-    const trafficLights = document.querySelectorAll('.traffic-light');
-    
-    for (let i = 0; i < 3; i ++) {
-        const lights = trafficLights[i].querySelectorAll('.light');
-        lights[1].classList.add('yellow-light');
-    }
-}
+  for (let i = 0; i < 3; i++) {
+    const lights = trafficLights[i].querySelectorAll(".light");
+    lights[1].classList.add("yellow-light");
+  }
+};
 
 const blinkGreenLights = () => {
-    const trafficLights = document.querySelectorAll('.traffic-light');
-    
-    for (let i = 0; i < 3; i ++) {
-        const lights = trafficLights[i].querySelectorAll('.light');
-        lights[2].classList.add('green-light');
-    }
-}
+  for (let i = 0; i < 3; i++) {
+    const lights = trafficLights[i].querySelectorAll(".light");
+    lights[2].classList.add("green-light");
+  }
+};
+*/
 
 const blinkLights = () => {
-    const trafficLights = document.querySelectorAll('.traffic-light');
-    clearLights();
-    setTimeout(blinkRedLights, 1000);
-    clearLights();
-    setTimeout(blinkYellowLights, 2000);
-    clearLights();
-    setTimeout(blinkGreenLights, 3000);
+  clearLights();
+  //   setInterval(blinkRedLights, 1000);
+  //   setInterval(blinkYellowLights, 1000);
+  //   setInterval(blinkGreenLights, 1000);
+  setTimeout(() => glowLights(RED), 1000);
+  setTimeout(() => glowLights(YELLOW), 2000);
+  setTimeout(() => glowLights(GREEN), 3000);
 };
 
 // 1. Button Press Event
-const startButton = document.querySelector('.start');
-startButton.addEventListener('click', () => {
-    console.log('Start Button');
-    blinkLights();
+const startButton = document.querySelector(".start");
+startButton.addEventListener("click", () => {
+  blinkLights();
 });
