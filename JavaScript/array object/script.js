af93be12f08a45190d931b53a36f5bbb023e5c07
Gardas Akash <134189310+akashgardas@users.noreply.github.com>
2025-08-19 09:58:51 +0000

    js array object methods

---
+++ JavaScript/array object/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Array object methods
+const arr = [1, 'akash', true];
+arr.push(2);
+console.log(arr);
+arr.pop(3);
+console.log(arr);
+console.log(arr.includes('akash'));
+arr.shift(1);
+console.log(arr);
